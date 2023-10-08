3f3ea9b87cb93360076b87dcd3e641e4b72e8ee6
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-10-08 09:08:16 +0530

    CoronaVirus Tracking Spring Boot application

---
+++ coronavirus-tracker/src/main/java/com/io/shreyash/coronavirustracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.io.shreyash.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+	}
+
+}
